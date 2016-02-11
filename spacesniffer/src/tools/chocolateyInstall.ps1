02dada4d5eb5e3615865a5b56384a67f2df8d662
gluck1331 <gluck1331@gmail.com>
2016-02-12 00:50:25 +0200

    Утилита для создания резервной копии настроек программ
    CloneApp - очень удобное решение для создания резервных копий настроек и
    профилей самых популярных приложений, что поможет Вам быстро
    восстановить все их основные параметры при переустановке системы или
    переносе на другой ПК. Периодически все пользователи сталкиваются в
    необходимостью установки/переустановки Windows, при этом каждый раз
    приходится инсталлировать и настраивать по-новой требуемые для работы
    программы. Этот процесс связан с рутинными операциями и немного
    утомляет, а если приложений много, то еще и занимает достаточное время.

---
+++ spacesniffer/src/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'spacesniffer' # arbitrary name for the package, used in messages
+$url = 'https://dl.dropboxusercontent.com/u/9319413/soft/x32/spacesniffer-1.3.0.0.zip' # download url
+$pclocation = $env:ProgramFiles+"\smalltools\"+$packageName
+
+Install-ChocolateyZipPackage $packageName $url $pclocation
