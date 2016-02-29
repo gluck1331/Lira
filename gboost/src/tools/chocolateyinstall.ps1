1488e62be9d22a31eee70c7d76dbd4431f298c2b
gluck1331 <gluck1331@gmail.com>
2016-02-29 10:06:20 +0200

    Утилита для оптимизации под производительность ПК для игр
    GBoost — бесплатная утилита для 32-битных и 64-разрядных операционных
    систем Microsoft Windows, которая предназначена для решения проблемы
    нехватки ресурсов за счёт отключения системных служб, модулей,
    виртуальных дисков и программ на всё время игрового процесса для
    настройки компьютера на максимальное быстродействие.

---
+++ gboost/src/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'gboost' # arbitrary name for the package, used in messages
+$installerType = 'exe'
+$url = 'https://dl.dropboxusercontent.com/u/9319413/soft/x32/gboost-1.0.3.0.zip' # download url
+$pclocation = $env:ProgramFiles+"\smalltools\"+$packageName
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+Install-ChocolateyZipPackage $packageName $url $pclocation $silentArgs
+
+$exeFilePath = get-childitem $pclocation -recurse -include *.$installerType | select -First 1
+Install-ChocolateyInstallPackage "$packageName" "$installerType" "$silentArgs" "$exeFilePath"
+
+new-item "$env:USERPROFILE\AppData\Local\LiveGBoost" -type directory
+
+Copy-Item $pclocation\DownloadList.boost -Destination $env:USERPROFILE\AppData\Local\LiveGBoost
